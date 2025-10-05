dfab80805a43ac5aacfa29cf5e8a7a338ee265fb
Azzam WIldan Maulana <azzamwildan5@gmail.com>
2025-10-05 12:51:19 +0700

    add: template python

---
+++ src/master/src/template.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+import rclpy
+from rclpy.node import Node 
+
+from loguru import logger
+
+class Template(Node):
+    def __init__(self):
+        super().__init__('template_node')
+        self.get_logger().info("Template Node has been started.")
+
+        # Example of a timer that calls a callback every second
+        self.timer = self.create_timer(1.0, self.timer_callback)
+
+    def timer_callback(self):
+        self.get_logger().info("Template Node is running...")
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    node_template = Template()
+
+    executor = rclpy.executors.MultiThreadedExecutor()
+    executor.add_node(node_template)
+    executor.spin()
+    
+if __name__ == '__main__':
+    main(sys.argv)
